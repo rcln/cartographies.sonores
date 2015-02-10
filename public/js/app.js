79df5854361ca6a2b7eec8aed10187fca351b21e
Filippo <admin@caio-corro.fr>
2015-02-10 18:57:28 +0100

    some stuffs

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,17 @@ $(function () {
     $.get('/data', function(languages) {
         LanguageStore.load(languages);
 
-        React.render(<List />, document.getElementById('content-list'));
-        React.render(<LeafletMap />, document.getElementById('content-map'));
+        var LanguageList = React.createClass({
+            render: function () {
+                return (
+                        <div>
+                            <div className="content-map"><LeafletMap className="content-map" /></div>
+                            <div className="content-list"><List className="content-list" /></div>
+                        </div>
+                       );
+            }
+        });
+
+        React.render(<LanguageList />, document.getElementById('content'));
     });
 });
