c6a1ee6746bc96f10760b76781e79a58b8ba2097
Caio Filippo Corro <admin@caio-corro.fr>
2015-09-22 17:10:41 +0200

    socketPath config

--- db.js
+++ db.js
@@ -1,12 +1,17 @@
 var mysql      = require('mysql');
 var config     = require('./config');
 
-var connection = mysql.createConnection({
+var param = {
   host     : config.db.host,
   user     : config.db.user,
   password : config.db.password,
   database : config.db.database
-});
+};
+
+if (config.db.socketPath != undefined)
+  param.socketPath: config.db.socketPath;
+
+var connection = mysql.createConnection(param);
  
 connection.connect(function(err) {
     if (err) throw err;
