518b46759f26f2ec578e01c34c64ee18f5eac0d4
Simon David HERNANDEZ <mail@sdavidhernandez.com>
2016-09-07 04:31:12 +0200

    Missing file, filter table

---
+++ public/js/list_search.js
@@ -0,0 +1,16 @@
+function searchKeyUp(e){
+    var search_string = $("#rechercher").val().toLowerCase();
+    if(search_string.length > 0){
+        var filtered_objs = document.languages_search.filter(function(objs){
+            return objs.text.search(search_string) > 0;
+        });
+        console.log(filtered_objs);
+        $("#table tbody tr").hide();
+        filtered_objs.forEach(function(obj, v){ 
+            $("#table tbody tr[data-reactid='" + obj.id + "']").show(); 
+        
+        });
+    }else{
+        $("#table tbody tr").show();
+    }
+}
